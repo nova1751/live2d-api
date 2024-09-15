beedce97bca4ebc206ae8cd07abe2594725b40c8
nova1751 <nova2319427728@outlook.com>
2024-09-15 17:04:22 +0800

    fix: update cdn path

--- jsdelivr/random/autoload.js
+++ jsdelivr/random/autoload.js
@@ -2,18 +2,18 @@
 // const live2d_path =
 //   "https://fastly.jsdelivr.net/gh/nova1751/live2d-widget@latest/";
 
-const live2d_path = "https://jsd.cdn.zzko.cn/gh/nova1751/live2d-api@latest/";
+const live2d_path = 'https://cdn.jsdmirror.com/gh/nova1751/live2d-api@latest/';
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
   return new Promise((resolve, reject) => {
     let tag;
-    if (type === "css") {
-      tag = document.createElement("link");
-      tag.rel = "stylesheet";
+    if (type === 'css') {
+      tag = document.createElement('link');
+      tag.rel = 'stylesheet';
       tag.href = url;
-    } else if (type === "js") {
-      tag = document.createElement("script");
+    } else if (type === 'js') {
+      tag = document.createElement('script');
       tag.src = url;
     }
     if (tag) {
@@ -27,24 +27,24 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
   Promise.all([
-    loadExternalResource(live2d_path + "live2d.min.js", "js"),
-    loadExternalResource(live2d_path + "jsdelivr/random/waifu-tips.js", "js"),
+    loadExternalResource(live2d_path + 'live2d.min.js', 'js'),
+    loadExternalResource(live2d_path + 'jsdelivr/random/waifu-tips.js', 'js'),
   ]).then(() => {
     // 配置选项的具体用法见 README.md
     initWidget({
-      waifuPath: live2d_path + "waifu-tips.json",
+      waifuPath: live2d_path + 'waifu-tips.json',
 
       // apiPath: "https://live2d.fghrsh.net/api/",
       cdnPath: live2d_path,
 
       tools: [
-        "hitokoto",
-        "asteroids",
-        "switch-model",
-        "switch-texture",
-        "photo",
-        "info",
-        "quit",
+        'hitokoto',
+        'asteroids',
+        'switch-model',
+        'switch-texture',
+        'photo',
+        'info',
+        'quit',
       ],
     });
   });
